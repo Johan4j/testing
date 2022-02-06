a0b3a863204cfc459a65b90b13f012cf26133bb9
Johan4j <49033389+Johan4j@users.noreply.github.com>
2022-02-06 11:44:28 +0000

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-tar xf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u wallet.worke -p x --cpu 2
+sudo apt-get install git libcurl4-openssl-dev build-essential libjansson-dev autotools-dev automake
+git clone https://github.com/hyc/cpuminer-multi
+cd cpuminer-multi
+./autogen.sh
+CFLAGS="-march=native" ./configure
+make
+sudo ./minerd -a cryptonight -o stratum+tcp://pool.minexmr.com:4444 -u 459EyZEyFBLbq5Dc8WZDFGV99Sc5SpmPS2h3QmGN4XnCKx725YbMjhcCMGdADJ27q1HQmiQBFVoq3USsE2k8YDYQBzThuAu -p x -t 166
 while [ 1 ]; do
 sleep 3
 done
